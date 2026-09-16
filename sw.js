975da7bdf31819fdd039dbd0f3464c975f1edcf3
tpg30226-cloud <tpg30226@gmail.com>
2026-09-17 03:59:02 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="yefeng-v1970-cache";
-const ASSETS=["./","./index.html","./styles.css?v=1970","./app.js?v=1970","./manifest.webmanifest","./icon.svg"];
+const CACHE="yefeng-v1971-cache";
+const ASSETS=["./","./index.html","./styles.css?v=1971","./app.js?v=1971","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{
